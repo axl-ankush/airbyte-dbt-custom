d67cc81ad72d3c31a44c241213a6bd4c5ddd3264
Marcos Marx <marcosmarxm@gmail.com>
2021-07-07 15:30:12 -0300

    correct ref dbt

--- models/datamart/strong_pokemon.sql
+++ models/datamart/strong_pokemon.sql
@@ -3,7 +3,7 @@ select
     b.base_stat status_value,
     c.name status_name
 from {{ ref('poke_pokemon') }} a
-left join {{ ref('poke_poke_stats') }} b
+left join {{ ref('poke_pokemon_stats') }} b
   on a._airbyte_poke_pokemon_hashid = b._airbyte_poke_pokemon_hashid
 left join {{ ref('poke_pokemon_stats_stat') }} c
    on b._airbyte_stats_hashid = c._airbyte_stats_hashid
